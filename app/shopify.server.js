bc0dbdd24b52d6728712b70bdeb7be00e811ab18
Vikas Prasad <prasadvvikas@gmail.com>
2025-05-22 16:07:05 +0530

    may be work this vikasd

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -27,7 +27,7 @@ const connectDB = async () => {
       throw new Error('MONGODB_URI is not defined in environment variables');
     }
     
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
