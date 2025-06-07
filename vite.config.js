504d2e4e2dd2c95431757ec69e89239246b65415
Vikas Prasad <prasadvvikas@gmail.com>
2025-06-07 12:45:37 +0530

    final changes 01

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from '@tailwindcss/vite';
+
   
 installGlobals({ nativeFetch: true });
 
@@ -64,7 +64,7 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
-    tailwindcss()
+
   ],
   build: {
     assetsInlineLimit: 0,
