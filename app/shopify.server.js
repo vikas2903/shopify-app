0c4c35c616c746fd5c95c2eac09d62083d520e9a
Vikas Prasad <prasadvvikas@gmail.com>
2025-07-02 13:47:03 +0530

    June 02 [11]

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -354,8 +354,7 @@ console.log("shopify.server.js loaded successfully 01");
 
 // Start Express server unless running in test environment
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on port ${PORT}`);
+// });
